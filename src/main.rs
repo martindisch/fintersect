2232c1f1cb35b200d81a4e80411d287485c84f8d
Martin Disch <martindisch@gmail.com>
2023-02-12 16:01:47 +0100

    Compare read and sort performance on a small chunk
    Sorting takes about 9 times as long as reading.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
+use env_logger::Env;
+use eyre::Result;
+use log::info;
 use std::{
     fs::File,
     io::{BufReader, Read},
 };
 
-use eyre::Result;
+const CHUNK_SIZE: usize = 2_usize.pow(30); // 1 GiB
 
 fn main() -> Result<()> {
-    let mut reader = BufReader::new(File::open("1.bin")?);
-    let mut buffer = [0; 4];
+    let env = Env::default().filter_or("RUST_LOG", "info");
+    env_logger::init_from_env(env);
 
-    let mut count = 0_usize;
-    while reader.read_exact(&mut buffer).is_ok() {
-        let _number = u32::from_le_bytes(buffer);
-        count += 1;
+    let mut reader = BufReader::new(File::open("1.bin")?);
+    let mut num_buffer = [0; 4];
+    let mut chunk_buffer = Vec::<u32>::with_capacity(CHUNK_SIZE);
+
+    info!("Reading chunk");
+
+    for _ in 0..CHUNK_SIZE {
+        if reader.read_exact(&mut num_buffer).is_ok() {
+            let number = u32::from_le_bytes(num_buffer);
+            chunk_buffer.push(number);
+        } else {
+            break;
+        }
     }
 
-    println!("{count}");
+    info!("Sorting chunk");
+
+    chunk_buffer.sort_unstable();
+
+    info!("Done");
 
     Ok(())
 }
