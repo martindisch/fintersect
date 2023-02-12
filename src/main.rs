d59bd42029f3b45790cdf2f11462bb76606a0f23
Martin Disch <martindisch@gmail.com>
2023-02-12 15:20:05 +0100

    Test reading a single file
    Runs in 53 seconds on my system.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
+use std::{
+    fs::File,
+    io::{BufReader, Read},
+};
+
 use eyre::Result;
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    let mut reader = BufReader::new(File::open("1.bin")?);
+    let mut buffer = [0; 4];
+
+    let mut count = 0_usize;
+    while reader.read_exact(&mut buffer).is_ok() {
+        let _number = u32::from_le_bytes(buffer);
+        count += 1;
+    }
+
+    println!("{count}");
 
     Ok(())
 }
